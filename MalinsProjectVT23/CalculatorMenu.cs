7d8d7a08748ceca9a3574f8a276ae57e132bba09
malin <mal3eine@hotmail.com>
2023-01-09 10:04:38 +0100

    Added Calculator menu

--- MalinsProjectVT23/CalculatorMenu.cs
+++ MalinsProjectVT23/CalculatorMenu.cs
@@ -1,62 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MalinsProjectVT23;
 
-namespace MalinsProjectVT23
+public class CalculatorMenu
 {
-    public class CalculatorMenu
+    public int ReturnSelectionFromMenu()
     {
-        public int ReturnSelectionFromMenu()
-        {
-            Console.Clear();
-            var endAlternative = 5;
-            Console.WriteLine(" Calculator Menu");
-            Console.WriteLine("*****************");//Lines.LineThreeStar();
-            Console.WriteLine(" 1. Add two numbers");
-            Console.WriteLine(" 2. Subtract two numbers");
-            Console.WriteLine(" 3. Multiplicate two numbers");
-            Console.WriteLine(" 4. Divide two numbers");
-            Console.WriteLine($" {endAlternative}. Modulus");
-            ReturnFromMenuClass.ExitMenu();
-            var sel = ReturnFromMenuClass.ReturnFromMenu(endAlternative);
-            return sel;
-        }
+        Console.Clear();
+        var endAlternative = 6;
+        Console.WriteLine(" Calculator Menu");
+        Console.WriteLine("*****************"); //Lines.LineThreeStar();
+        Console.WriteLine(" 1. Add two numbers");
+        Console.WriteLine(" 2. Subtract two numbers");
+        Console.WriteLine(" 3. Multiplicate two numbers");
+        Console.WriteLine(" 4. Divide two numbers");
+        Console.WriteLine(" 5. Square Root");
+        Console.WriteLine($" {endAlternative}. Modulus");
+        ReturnFromMenuClass.ExitMenu();
+        var sel = ReturnFromMenuClass.ReturnFromMenu(endAlternative);
+        return sel;
+    }
 
-        public void LoopMenu(int selectedFromMenu) //ApplicationDbContext dbContext)
-        {
-            var loop = true;
-            while (loop)
-                switch (selectedFromMenu)
+    public void LoopMenu(int selectedFromMenu) //ApplicationDbContext dbContext)
+    {
+        var loop = true;
+        while (loop)
+            switch (selectedFromMenu)
+            {
+                case 1:
                 {
-                    case 1:
-                        {
-                          //add
-                            break;
-                        }
-                    case 2:
-                        {
-                            //subtract
-                            break;
-                        }
-                    case 3:
-                    {
-                        //multi
-                        break;
-                        }
-                    case 4:
-                    {
-                        //divide
-                        break;
-                    }
-                    case 5:
-                    {
-                        //modulus
-                        break;
-                    }
+                    //add
+                    break;
                 }
-        }
+                case 2:
+                {
+                    //subtract
+                    break;
+                }
+                case 3:
+                {
+                    //multi
+                    break;
+                }
+                case 4:
+                {
+                    //divide
+                    break;
+                }
+                case 5:
+                {
+                    //modulus
+                    break;
+                }
+            }
     }
 }
-
