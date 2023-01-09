a35d1498c67f87b2b0bc018179384560b5798d8b
malin <mal3eine@hotmail.com>
2023-01-09 11:04:31 +0100

    #12 Created data folder and classes in folder: Calculation, Shape, ShapeResults and RockScissorPaperGame

---
+++ MalinsProjectVT23/Data/Calculation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalinsProjectVT23.Data
+{
+    public class Calculation
+    {
+        public decimal Input1 { get; set; }
+        public decimal Input2 { get; set; }
+        public decimal Result { get; set; }
+    }
+}
---
+++ MalinsProjectVT23/Data/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalinsProjectVT23.Data
+{
+    internal class Shape
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
