21b435cc0c4dc8c2ff263981c5402784bbf629b5
malin <mal3eine@hotmail.com>
2023-01-09 16:06:28 +0100

    #23 Added SquareRoot and Modulus methods in Calculate

--- ClassLibraryCalculations/Calculate.cs
+++ ClassLibraryCalculations/Calculate.cs
@@ -1,4 +1,6 @@
-﻿namespace ClassLibraryCalculations
+﻿using ClassLibraryErrorHandling;
+
+namespace ClassLibraryCalculations
 {
     public class Calculate
     {
@@ -20,13 +22,36 @@
             {
                 while (true)
                 {
-                    if (num2 != 0) return num1 / num2;
+                    if (num2 != 0) 
+                        return num1 / num2;
+
                     Console.WriteLine("The number to divide with can not be zero. Try again");
                     Console.Write("Number to be divided: ");
-                    num1 = Convert.ToInt32(Console.ReadLine()); //errorhandling
+                    num1 = ErrorHandling.TryInt(); 
                     Console.Write("Number to divide with: ");
-                    num2 = Convert.ToInt32(Console.ReadLine()); //errorhandling
+                    num2 = ErrorHandling.TryInt(); 
                 }
+            }
+            public int Modulus(int num1, int num2)
+            {
+                while (true)
+                {
+                    if (num2 != 0)
+                    {
+                        return (Math.Abs(num1 + num2) + num1) % num2; ///trycatch?
+                    }
+
+                    Console.WriteLine("The number to divide with can not be zero. Try again");
+                    Console.Write("Number to be divided: ");
+                    num1 = ErrorHandling.TryInt();
+                    Console.Write("Number to divide with: ");
+                    num2 = ErrorHandling.TryInt();
+                }
+            }
+            public double SquareRoot(double numberToCalculateSquareRoot)
+            {
+                var num = Math.Sqrt(numberToCalculateSquareRoot);
+                return numberToCalculateSquareRoot;
             }
             //design pattern strategy pattern: byta ut en viss operation(metoderna ovan) mot en klass istället. 
         }
