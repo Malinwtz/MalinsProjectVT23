3cf0e1fa0e13e49833202a23f97c9ae8dedbe3d4
malin <mal3eine@hotmail.com>
2023-01-10 10:46:05 +0100

    #25 Created ClassLibraryStrings with classes Action and Line

---
+++ ClassLibraryStrings/Action.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassLibraryStrings;
+
+public class Action
+{
+    public static void PressEnterToContinue()
+    {
+        Console.Write("\n Tryck på enter för att fortsätta...");
+        Console.ReadKey();
+    }
+
+    public static void Successful(string text)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($" {text}");
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+
+    public static void NotSuccessful(string text)
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(text);
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+}
---
+++ ClassLibraryStrings/Line.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassLibraryStrings
+{
+    public class Line
+    {
+        public static void LineOneHyphen()
+        {
+            Console.WriteLine("------------------------------------------------------" +
+                              "-----------------------------------------------------------------");
+        }
+
+        public static void LineTwoEqual()
+        {
+            Console.WriteLine("======================================================" +
+                              "==================================================================" +
+                              Environment.NewLine);
+        }
+
+        
+        public static void LineThreeStar()
+        {
+            Console.WriteLine("******************************************************" +
+                              "******************************************************************" +
+                              Environment.NewLine);
+        }
+    }
+}
