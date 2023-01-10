49846a0eb23bf6971a09856b0251d899ae3b5c55
malin <mal3eine@hotmail.com>
2023-01-10 16:11:08 +0100

    #28 Created Shapes menu

---
+++ MalinsProjectVT23/ShapeController/ShapeMenu.cs
@@ -0,0 +1,60 @@
+﻿using ClassLibraryCalculations;
+using ClassLibraryErrorHandling;
+using ClassLibraryStrings;
+using MalinsProjectVT23.Data;
+using MalinsProjectVT23.MainMenuController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalinsProjectVT23.ShapeController
+{
+    public class ShapeMenu
+    {
+        public int ReturnSelectionFromMenu()
+        {
+            Console.Clear();
+            var endAlternative = 4;
+            Console.WriteLine(" Shape Menu");
+            Line.LineThreeStar();
+            Console.WriteLine(" 1. Rectangle");
+            Console.WriteLine(" 2. Parallelogram");
+            Console.WriteLine(" 3. Triangle");
+            Console.WriteLine($" {endAlternative}. Rhombus");
+            ReturnFromMenuClass.ExitMenu();
+            var sel = ReturnFromMenuClass.ReturnFromMenu(endAlternative);
+            return sel;
+        }
+
+        public void LoopMenu(int selectedFromMenu, ApplicationDbContext dbContext)
+        {
+            var loop = true;
+            while (loop)
+                switch (selectedFromMenu)
+                {
+                    case 1:
+                        {
+                            //visa shape
+                            break;
+                        }
+                    case 2:
+                        {
+                            //skapa shape
+                            break;
+                        }
+                    case 3:
+                        {
+                            //ändra shape
+                            break;
+                        }
+                    case 4:
+                    {
+                        //ta bort shape
+                    }
+                        break;
+                }
+        }
+    }
+}
