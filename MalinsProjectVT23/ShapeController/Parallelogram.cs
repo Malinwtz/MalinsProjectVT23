aaa8f356ae3e2644409e818b53ed1fa27f3c866e
malin <mal3eine@hotmail.com>
2023-01-11 15:21:00 +0100

    #37 Code cleanup parallelogram

--- MalinsProjectVT23/ShapeController/Parallelogram.cs
+++ MalinsProjectVT23/ShapeController/Parallelogram.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MalinsProjectVT23.Interface;
+﻿using MalinsProjectVT23.Interface;
 
-namespace MalinsProjectVT23.ShapeController
+namespace MalinsProjectVT23.ShapeController;
+
+public class Parallelogram : IShape
 {
-    public class Parallelogram : IShape
+    public decimal CalculateArea(decimal length, decimal height)
     {
-        public decimal CalculateArea(decimal length, decimal height)
-        {
-            var areaOfParallelogram = (length * height);
-            return areaOfParallelogram;
-        }
+        var areaOfParallelogram = length * height;
+        return areaOfParallelogram;
+    }
 
-        public decimal CalculateCircumference(decimal length, decimal height)
-        {
-            var circumferenceOfParallelogram = (length + height) * 2;
-            return circumferenceOfParallelogram;
-        }
+    public decimal CalculateCircumference(decimal length, decimal height)
+    {
+        var circumferenceOfParallelogram = (length + height) * 2;
+        return circumferenceOfParallelogram;
     }
 }
