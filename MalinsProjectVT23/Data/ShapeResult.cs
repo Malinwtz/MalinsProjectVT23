d5da927e4b07b36aac8823a38ff29feb83c016e0
malin <mal3eine@hotmail.com>
2023-01-14 15:07:43 +0100

    #78b deleted annotations decimal

--- MalinsProjectVT23/Data/ShapeResult.cs
+++ MalinsProjectVT23/Data/ShapeResult.cs
@@ -6,10 +6,10 @@ namespace MalinsProjectVT23.Data;
 public class ShapeResult
 {
     [Key] public int ShapeResultId { get; set; }
-    [Required][Column("HeightInCentimeter", TypeName = "decimal(38, 38)")] public decimal Height { get; set; }
-    [Required][Column("LengthInCentimeter", TypeName = "decimal(38, 38)")] public decimal Length { get; set; }
-    [Required][Column("CircumferenceInCentimeter", TypeName = "decimal(38, 38)")] public decimal Circumference { get; set; }
-    [Required][Column("AreaInSquareCentimeter", TypeName = "decimal(38, 38)")] public decimal Area { get; set; }
+    [Required][Column("HeightInCentimeter")] public decimal Height { get; set; }
+    [Required][Column("LengthInCentimeter")] public decimal Length { get; set; }
+    [Required][Column("CircumferenceInCentimeter")] public decimal Circumference { get; set; }
+    [Required][Column("AreaInSquareCentimeter")] public decimal Area { get; set; }
     [Required] public DateTime ResultDate { get; set; }
     [Required] public Shape Shape { get; set; }
 }
---
+++ MalinsProjectVT23/Migrations/20230114140432_Deleted decimal range.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MalinsProjectVT23.Migrations
+{
+    public partial class Deleteddecimalrange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "LengthInCentimeter",
+                table: "ShapeResults",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,38)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "HeightInCentimeter",
+                table: "ShapeResults",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,38)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CircumferenceInCentimeter",
+                table: "ShapeResults",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,38)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AreaInSquareCentimeter",
+                table: "ShapeResults",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,38)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Result",
+                table: "Calculations",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,38)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Input2",
+                table: "Calculations",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,38)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Input1",
+                table: "Calculations",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,38)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "LengthInCentimeter",
+                table: "ShapeResults",
+                type: "decimal(38,38)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "HeightInCentimeter",
+                table: "ShapeResults",
+                type: "decimal(38,38)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CircumferenceInCentimeter",
+                table: "ShapeResults",
+                type: "decimal(38,38)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AreaInSquareCentimeter",
+                table: "ShapeResults",
+                type: "decimal(38,38)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Result",
+                table: "Calculations",
+                type: "decimal(38,38)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Input2",
+                table: "Calculations",
+                type: "decimal(38,38)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Input1",
+                table: "Calculations",
+                type: "decimal(38,38)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
